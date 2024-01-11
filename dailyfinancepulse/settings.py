41300484e0b19b9b50fd398e0f22f8d73b25370d
ZeroCool989 <almirdumisic@gmail.com>
2024-01-11 11:36:39 +0000

    Add allowed host to check if the app runs on deplyed geroku app

--- dailyfinancepulse/settings.py
+++ dailyfinancepulse/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dailyfinancepulse.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['dailyfinancepulse-9ee5b1b4407f.herokuapp.com', 'localhost']
 
 
 # Application definition
